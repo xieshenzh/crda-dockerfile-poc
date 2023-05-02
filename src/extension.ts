39e7f4900d8830d6a3ec57c9cd97b443d3404433
xiezhang <xiezhang@redhat.com>
2023-05-02 16:59:07 -0400

    Implement diagnostics for dockerfile

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,46 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const diagnosticCollection = vscode.languages.createDiagnosticCollection('dockerfile');
+	
+	context.subscriptions.push(diagnosticCollection);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "crda-dockerfile-poc" is now active!');
+	if (vscode.window.activeTextEditor) {
+		updateDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
+	}
+	context.subscriptions.push(
+		vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				updateDiagnostics(editor.document, diagnosticCollection);
+			}
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('crda-dockerfile-poc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello D from crda-dockerfile-poc!');
-	});
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(e => updateDiagnostics(e.document, diagnosticCollection))
+	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.workspace.onDidCloseTextDocument(doc => diagnosticCollection.delete(doc.uri))
+	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
+	if (document && path.basename(document.uri.fsPath) === 'sample-demo.rs') {
+		collection.set(document.uri, [{
+			code: '',
+			message: 'cannot assign twice to immutable variable `x`',
+			range: new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 10)),
+			severity: vscode.DiagnosticSeverity.Error,
+			source: '',
+			relatedInformation: [
+				new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
+			]
+		}]);
+	} else {
+		collection.clear();
+	}
+}
