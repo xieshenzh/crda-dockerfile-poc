c95cbe0863595718c3283b97508e3a305464a751
xiezhang <xiezhang@redhat.com>
2023-06-06 23:28:37 -0400

    Retrieve vulnerability data from quay.io for single arch images

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {Diagnostic} from 'vscode';
+import {Diagnostic, DiagnosticSeverity} from 'vscode';
 import {DockerfileParser} from 'dockerfile-ast';
+import * as rm from 'typed-rest-client/RestClient';
+import * as httpm from 'typed-rest-client/HttpClient';
 import Docker = require('dockerode');
 
-const imageRegex = new RegExp('[q|Q][u|U][a|A][y|Y]\\.[i|I][o|O]\\/([^\\/.]+\\/)?[^\\/.]+(:.+)?');
-const digestRegex = new RegExp('Digest: sha256:[A-Fa-f{64}]');
+let imageRegex = new RegExp('[q|Q][u|U][a|A][y|Y]\\.[i|I][o|O]\\/([^\\/.]+\\/)?[^\\/.]+(:.+)?');
+let digestRegex = new RegExp('Digest: sha256:[A-Fa-f0-9{64}]');
+
+let baseUrl: string = 'https://quay.io';
+let restc: rm.RestClient = new rm.RestClient('crda', baseUrl);
+
+let severities: string[] = ['Critical', 'High', 'Medium', 'Low', 'Unknown'];
 
 interface PullStatus {
     status: string
+}
+
+interface Vulnerability {
+    Name: string,
+    Severity: string,
+}
+
+interface Feature {
+    Name: string,
+    Vulnerabilities: Vulnerability[]
+}
+
+interface Layer {
+    Name: string,
+    Features: Feature[]
+}
+
+interface Data {
+    Layer: Layer
+}
+
+interface Vulnerabilities {
+    status: string,
+    data: Data
 }
 
 // This method is called when your extension is activated
@@ -81,7 +112,7 @@ async function updateDiagnostics(document: vscode.TextDocument): Promise<Diagnos
                         } else {
                             resolve(result);
                         }
-                    })).then(result => {
+                    })).then(async result => {
                         if (isError(result)) {
                             console.log(result);
                             let range = from.getImageRange();
@@ -101,17 +132,24 @@ async function updateDiagnostics(document: vscode.TextDocument): Promise<Diagnos
                             for (let status of results) {
                                 let s = status as PullStatus;
                                 if (digestRegex.test(s.status)) {
-                                    let range = from.getImageRange();
-                                    diagnostics.push({
-                                        code: '',
-                                        message: getImageManifestAddress(image!, s.status),
-                                        range: new vscode.Range(
-                                            new vscode.Position(range?.start.line!, range?.start.character!), new vscode.Position(range?.end.line!, range?.end.character!),
-                                        ),
-                                        severity: vscode.DiagnosticSeverity.Error,
-                                        source: '',
-                                        relatedInformation: []
-                                    });
+                                    await getImageManifestAddress(image!, s.status).then(
+                                        async result => {
+                                            if (result[0] === null || result[1] === null) {
+                                                return;
+                                            }
+
+                                            let range = from.getImageRange();
+                                            diagnostics.push({
+                                                code: '',
+                                                message: result[0],
+                                                range: new vscode.Range(new vscode.Position(range?.start.line!, range?.start.character!),
+                                                    new vscode.Position(range?.end.line!, range?.end.character!),),
+                                                severity: result[1],
+                                                source: '',
+                                                relatedInformation: []
+                                            });
+                                        }
+                                    );
                                     break;
                                 }
                             }
@@ -139,10 +177,54 @@ async function updateDiagnostics(document: vscode.TextDocument): Promise<Diagnos
     }
 }
 
-function getImageManifestAddress(image: string, digest: string): string {
+async function getImageManifestAddress(image: string, digest: string): Promise<[string, DiagnosticSeverity]> {
     let repo = image.slice(8, image.lastIndexOf(":"));
     let sha = digest.slice(8);
-    return 'https://quay.io/repository/' + repo + '/manifest/' + sha + '?tab=vulnerabilities';
+    let path = '/api/v1/repository/' + repo + '/manifest/' + sha + '/security';
+    let options: rm.IRequestOptions = <rm.IRequestOptions>{
+        queryParameters: {
+            params: {
+                vulnerabilities: 'true',
+            }
+        }
+    };
+    try {
+        let restRes: rm.IRestResponse<Vulnerabilities> = await restc.get<Vulnerabilities>(path, options);
+        if (restRes.statusCode === httpm.HttpCodes.NotFound) {
+            return ["Status Code 404 - Request to quay.io failed", vscode.DiagnosticSeverity.Error];
+        }
+
+        let vulMap = new Map<string, string>();
+        for (let feature of restRes.result.data.Layer.Features) {
+            for (let vul of feature.Vulnerabilities) {
+                vulMap.set(vul.Name, vul.Severity);
+            }
+        }
+
+        if (vulMap.size === 0) {
+            return [null, null];
+        }
+
+        let message: string = "";
+        for (let severity of severities) {
+            for (let [n, s] of vulMap) {
+                if (s === severity) {
+                    message += (n + ": " + s + "\n");
+                }
+            }
+        }
+
+        let severity: DiagnosticSeverity = DiagnosticSeverity.Warning;
+        for (let [n, s] of vulMap) {
+            if (s === "Critical" || s=== "High") {
+                severity = DiagnosticSeverity.Error;
+            }
+        }
+
+        return [message, severity];
+    } catch (error) {
+        return ["Status Code " + error['statusCode'] + " - " + error.message + ", Result: " + error['result'], vscode.DiagnosticSeverity.Error];
+    }
 }
 
 const isError = (err: unknown): err is Error => err instanceof Error;
