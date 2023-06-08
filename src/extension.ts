963fe1ea3e4dc397e4ba7171db2349b2dd7b5f98
xiezhang <xiezhang@redhat.com>
2023-06-08 00:36:36 -0400

    Parse quay.io image digest for multi-arch images

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as rm from 'typed-rest-client/RestClient';
 import * as httpm from 'typed-rest-client/HttpClient';
 import Docker = require('dockerode');
 
-let imageRegex = new RegExp('[q|Q][u|U][a|A][y|Y]\\.[i|I][o|O]\\/([^\\/.]+\\/)?[^\\/.]+(:.+)?');
+let imageRegex = new RegExp('[q|Q][u|U][a|A][y|Y]\\.[i|I][o|O]\\/([^\\/.]+\\/)?[^\\/.]+(:.+)?'); //todo
 let digestRegex = new RegExp('Digest: sha256:[A-Fa-f0-9{64}]');
 
 let baseUrl: string = 'https://quay.io';
@@ -17,6 +17,12 @@ let severities: string[] = ['Critical', 'High', 'Medium', 'Low', 'Unknown'];
 
 interface PullStatus {
     status: string
+}
+
+interface Manifest {
+    digest: string,
+    is_manifest_list: boolean,
+    manifest_data: string
 }
 
 interface Vulnerability {
@@ -95,67 +101,37 @@ export function deactivate() {
 async function updateDiagnostics(document: vscode.TextDocument): Promise<Diagnostic[]> {
     if (document.languageId === "dockerfile") {
         let dockerfile = DockerfileParser.parse(document.getText());
-
         let froms = dockerfile.getFROMs();
 
-        const docker = new Docker();
         let diagnostics: Diagnostic[] = [];
         for (let from of froms) {
             let image = from.getImage();
             console.log(image);
             if (image && imageRegex.test(image)) {
                 try {
-                    const pullStream = await docker.pull(image);
-                    await new Promise(resolve => docker.modem.followProgress(pullStream, (error, result) => {
-                        if (error) {
-                            resolve(error);
-                        } else {
-                            resolve(result);
-                        }
-                    })).then(async result => {
-                        if (isError(result)) {
-                            console.log(result);
-                            let range = from.getImageRange();
-                            diagnostics.push({
-                                code: '',
-                                message: (<Error>result).message,
-                                range: new vscode.Range(
-                                    new vscode.Position(range?.start.line!, range?.start.character!), new vscode.Position(range?.end.line!, range?.end.character!),
-                                ),
-                                severity: vscode.DiagnosticSeverity.Error,
-                                source: '',
-                                relatedInformation: []
-                            });
-                        } else {
-                            console.log(result);
-                            let results = result as object[];
-                            for (let status of results) {
-                                let s = status as PullStatus;
-                                if (digestRegex.test(s.status)) {
-                                    await getImageManifestAddress(image!, s.status).then(
-                                        async result => {
-                                            if (result[0] === null || result[1] === null) {
-                                                return;
-                                            }
+                    let digests = await getImageDigest(image);
+                    let [repo, digest] = await getImageManifestRef(digests);
+                    let [message, severity] = await getImageVulnerabilities(repo, digest);
 
-                                            let range = from.getImageRange();
-                                            diagnostics.push({
-                                                code: '',
-                                                message: result[0],
-                                                range: new vscode.Range(new vscode.Position(range?.start.line!, range?.start.character!),
-                                                    new vscode.Position(range?.end.line!, range?.end.character!),),
-                                                severity: result[1],
-                                                source: '',
-                                                relatedInformation: []
-                                            });
-                                        }
-                                    );
-                                    break;
-                                }
-                            }
-                        }
+                    let messageSeverity: DiagnosticSeverity;
+                    if (severity === "Critical" || severity === "High") {
+                        messageSeverity = DiagnosticSeverity.Error;
+                    } else if (severity !== undefined) {
+                        messageSeverity = DiagnosticSeverity.Warning;
+                    }
+
+                    let range = from.getImageRange();
+                    diagnostics.push({
+                        code: '',
+                        message: message,
+                        range: new vscode.Range(new vscode.Position(range?.start.line!, range?.start.character!),
+                            new vscode.Position(range?.end.line!, range?.end.character!),),
+                        severity: messageSeverity,
+                        source: '',
+                        relatedInformation: []
                     });
-                } catch (error) {
+                } catch
+                    (error) {
                     console.error(error);
                     let range = from.getImageRange();
                     diagnostics.push({
@@ -177,10 +153,8 @@ async function updateDiagnostics(document: vscode.TextDocument): Promise<Diagnos
     }
 }
 
-async function getImageManifestAddress(image: string, digest: string): Promise<[string, DiagnosticSeverity]> {
-    let repo = image.slice(8, image.lastIndexOf(":"));
-    let sha = digest.slice(8);
-    let path = '/api/v1/repository/' + repo + '/manifest/' + sha + '/security';
+async function getImageVulnerabilities(repository: string, digest: string): Promise<[string, string]> {
+    let path = '/api/v1/repository/' + repository + '/manifest/' + digest + '/security';
     let options: rm.IRequestOptions = <rm.IRequestOptions>{
         queryParameters: {
             params: {
@@ -188,43 +162,92 @@ async function getImageManifestAddress(image: string, digest: string): Promise<[
             }
         }
     };
-    try {
-        let restRes: rm.IRestResponse<Vulnerabilities> = await restc.get<Vulnerabilities>(path, options);
-        if (restRes.statusCode === httpm.HttpCodes.NotFound) {
-            return ["Status Code 404 - Request to quay.io failed", vscode.DiagnosticSeverity.Error];
-        }
 
-        let vulMap = new Map<string, string>();
-        for (let feature of restRes.result.data.Layer.Features) {
-            for (let vul of feature.Vulnerabilities) {
-                vulMap.set(vul.Name, vul.Severity);
-            }
-        }
-
-        if (vulMap.size === 0) {
-            return [null, null];
-        }
-
-        let message: string = "";
-        for (let severity of severities) {
-            for (let [n, s] of vulMap) {
-                if (s === severity) {
-                    message += (n + ": " + s + "\n");
-                }
-            }
-        }
-
-        let severity: DiagnosticSeverity = DiagnosticSeverity.Warning;
-        for (let [n, s] of vulMap) {
-            if (s === "Critical" || s=== "High") {
-                severity = DiagnosticSeverity.Error;
-            }
-        }
-
-        return [message, severity];
-    } catch (error) {
-        return ["Status Code " + error['statusCode'] + " - " + error.message + ", Result: " + error['result'], vscode.DiagnosticSeverity.Error];
+    let restRes: rm.IRestResponse<Vulnerabilities> = await restc.get<Vulnerabilities>(path, options);
+    if (restRes.statusCode === httpm.HttpCodes.NotFound) {
+        return ["Status Code 404 - Request to quay.io failed", "Critical"];
     }
+
+    let vulMap = new Map<string, string>();
+    for (let feature of restRes.result.data.Layer.Features) {
+        for (let vul of feature.Vulnerabilities) {
+            vulMap.set(vul.Name, vul.Severity);
+        }
+    }
+
+    if (vulMap.size === 0) {
+        return [undefined, undefined];
+    }
+
+    let severitySet = new Set<string>();
+    let message: string = "";
+    for (let severity of severities) {
+        for (let [n, s] of vulMap) {
+            if (s === severity) {
+                severitySet.add(s);
+                message += (n + ": " + s + "\n");
+            }
+        }
+    }
+
+    for (let severity of severities) {
+        if (severitySet.has(severity)) {
+            return [message, severity];
+        }
+    }
+
+    return [message, undefined];
 }
+
+async function getImageDigest(imgName: string): Promise<string[]> {
+    let docker = new Docker();
+    let pullStream = await docker.pull(imgName);
+    await new Promise(resolve => docker.modem.followProgress(pullStream, (error, result) => {
+        if (error) {
+            resolve(error);
+        } else {
+            resolve(result);
+        }
+    })).then(async result => {
+        if (isError(result)) {
+            console.log(result);
+            throw result;
+        } else {
+            console.log(result);
+        }
+    });
+
+    let image = docker.getImage(imgName);
+    let info = await image.inspect();
+    return info.RepoDigests;
+}
+
+async function getImageManifestRef(digests: string[]): Promise<[string, string]> {
+    let repository: string;
+    let manifestData: string;
+    let manifestRef: string;
+    for (let digest of digests) {
+        let at = digest.indexOf("@");
+        let repo = digest.slice(8, at);
+        let ref = digest.slice(at + 1);
+        let path = '/api/v1/repository/' + repo + '/manifest/' + ref;
+        let restRes: rm.IRestResponse<Manifest> = await restc.get<Manifest>(path);
+        if (restRes.statusCode !== httpm.HttpCodes.NotFound) {
+            repository = repo;
+            if (restRes.result.is_manifest_list) {
+                manifestData = restRes.result.manifest_data;
+            } else {
+                manifestRef = restRes.result.digest;
+            }
+        }
+    }
+
+    if (repository !== undefined && manifestRef !== undefined) {
+        return [repository, manifestRef];
+    }
+
+    //todo
+}
+
 
 const isError = (err: unknown): err is Error => err instanceof Error;
